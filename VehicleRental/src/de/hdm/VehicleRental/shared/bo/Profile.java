2785f865775643adb62ee86578475a0cfae79cfa
Flex <fw038@hdm-stuttgart.de>
2017-05-12 16:54:11 +0200

    Fehlermeldungen behoben, verschiedene Methoden bearbeitet um lauffähigen Code zu erhalten, bspw. Nachname und Vorname zu Name zusammengefasst

--- VehicleRental/src/de/hdm/VehicleRental/shared/bo/Profile.java
+++ VehicleRental/src/de/hdm/VehicleRental/shared/bo/Profile.java
@@ -1,28 +1,29 @@
 package de.hdm.VehicleRental.shared.bo;
 
-import datatype.Long;
-import java.lang.Object;
-
 public class Profile extends BusinessObject {
 
 	/**
 	 * 
 	 */
-	private String driversLicence;
+	private int driversLicence;
 	/**
 	 * 
-	   * Der Nachname des Kunden.
-	   
-	 */
-	private String lastName;
-	/**
+	 * Der Nachname des Kunden.
 	 * 
 	 */
-	private EDate birthday;
+	private String name;
 	/**
 	 * 
 	 */
-	private static final Long serialVersionUID;
+private double balance;
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
 	/**
 	 * 
 	 */
@@ -35,151 +36,93 @@ public class Profile extends BusinessObject {
 	 * 
 	 */
 	private String adress;
+	
+	
+
 	/**
 	 * 
-	   * Der Vorname des Kunden.
-	   
+	 * @return
 	 */
-	private String firstName;
+	public int getDriversLicence() {
+		return driversLicence;
+	}
+
 	/**
 	 * 
-	 * @return 
+	 * @param driversLicenceID
 	 */
-	public String getDriversLicence() {
-	 	 return driversLicence; 
+	public void setDriversLicence(int driversLicence) {
+		this.driversLicence = driversLicence;
 	}
+
 	/**
 	 * 
-	 * @param driversLicenceID 
+	 * Auslesen des Nachnamens.
+	 * 
+	 * @return
 	 */
-	public void setDriversLicence(String driversLicence) { 
-		 this.driversLicence = driversLicence; 
+	public String getName() {
+		return name;
 	}
+
 	/**
 	 * 
-	   * Auslesen des Nachnamens.
-	   
-	 * @return 
+	 * Setzen des Nachnamens.
+	 * 
+	 * @param name
 	 */
-	public String getLastName() {
-	 	 return lastName; 
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	/**
 	 * 
-	   * Setzen des Nachnamens.
-	   
-	 * @param name 
+	 * @return
 	 */
-	public void setLastName(String lastName) { 
-		 this.lastName = lastName; 
-	}
-	/**
-	 * 
-	 * @return 
-	 */
-	public EDate getBirthday() {
-	 	 return birthday; 
-	}
-	/**
-	 * 
-	 * @param birthday 
-	 */
-	public void setBirthday(EDate birthday) { 
-		 this.birthday = birthday; 
-	}
-	/**
-	 * Getter of serialVersionUID
-	 */
-	public Long getSerialVersionUID() {
-	 	 return serialVersionUID; 
-	}
-	/**
-	 * Setter of serialVersionUID
-	 */
-	public void setSerialVersionUID(Long serialVersionUID) { 
-		 this.serialVersionUID = serialVersionUID; 
-	}
-	/**
-	 * 
-	 * @return 
-	 */
+
 	public String getGender() {
-	 	 return gender; 
+		return gender;
 	}
+
 	/**
 	 * 
-	 * @param Gender 
+	 * @param Gender
 	 */
-	public void setGender(String gender) { 
-		 this.gender = gender; 
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
+
 	/**
 	 * 
-	 * @return 
+	 * @return
 	 */
 	public String getEmail() {
-	 	 return email; 
+		return email;
 	}
+
 	/**
 	 * 
-	 * @param email 
+	 * @param email
 	 */
-	public void setEmail(String email) { 
-		 this.email = email; 
+	public void setEmail(String email) {
+		this.email = email;
 	}
+
 	/**
 	 * 
-	 * @return 
+	 * @return
 	 */
 	public String getAdress() {
-	 	 return adress; 
+		return adress;
 	}
+
 	/**
 	 * 
-	 * @param adress 
+	 * @param adress
 	 */
-	public void setAdress(String adress) { 
-		 this.adress = adress; 
+	public void setAdress(String adress) {
+		this.adress = adress;
 	}
-	/**
-	 * 
-	   * Auslesen des Vornamens.
-	   
-	 * @return 
-	 */
-	public String getFirstName() {
-	 	 return firstName; 
-	}
-	/**
-	 * 
-	   * Setzen des Vornamens.
-	   
-	 * @param name 
-	 */
-	public void setFirstName(String firstName) { 
-		 this.firstName = firstName; 
-	}
-	/**
-	 * 
-	   * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
-	   * Diese besteht aus dem Text, der durch die <code>toString()</code>-Methode
-	   * der Superklasse erzeugt wird, ergänzt durch den Vor- und Nachnamen des 
-	   * jeweiligen Kunden.
-	   
-	 * @return 
-	 */
-	public String toString() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param o 
-	 * @return 
-	 */
-	public boolean equals(Object o) { 
-		// TODO Auto-generated method
-		return false;
-	 } 
+
 
 }
