647b09a62649752f31c603ac1d4c5d45cfa1d954
Flex <fw038@hdm-stuttgart.de>
2017-05-12 16:56:37 +0200

    Verschiedene Fehler behoben, bspw. packages und imports. Integer zu int geändert Methode Equals gelöscht Methode Hashcode gelöscht ToString gelöscht

--- VehicleRental/src/de/hdm/VehicleRental/shared/bo/BusinessObject.java
+++ VehicleRental/src/de/hdm/VehicleRental/shared/bo/BusinessObject.java
@@ -1,9 +1,6 @@
 package de.hdm.VehicleRental.shared.bo;
 
 import java.io.Serializable;
-import datatype.Long;
-import datatype.Integer;
-import datatype.Boolean;
 import java.lang.Object;
 
 /**
@@ -17,14 +14,14 @@ import java.lang.Object;
  * besitzt, die man in einer relationalen Datenbank auch als Primärschlüssel
  * bezeichnen würde. Fernen ist jedes <code>BusinessObject</code> als
  * {@link Serializable} gekennzeichnet. Durch diese Eigenschaft kann jedes
- * <code>BusinessObject</code> automatisch in eine textuelle Form überführt und
- * z.B. zwischen Client und Server transportiert werden. Bei GWT RPC ist diese
- * textuelle Notation in JSON (siehe http://www.json.org/) kodiert.
+ * <code>BusinessObject</code> automatisch in eine textuelle Form überführt
+ * und z.B. zwischen Client und Server transportiert werden. Bei GWT RPC ist
+ * diese textuelle Notation in JSON (siehe http://www.json.org/) kodiert.
  * </p>
  * 
  * @author thies
  * @version 1.0
- 
+ * 
  * @author
  */
 public abstract class BusinessObject implements Serializable {
@@ -32,96 +29,83 @@ public abstract class BusinessObject implements Serializable {
 	/**
 	 * 
 	 */
-	private static final Long serialVersionUID;
+	private static final long serialVersionUID = 1L;
 	/**
 	 * 
-	   * Die eindeutige Identifikationsnummer einer Instanz dieser Klasse.
-	   
 	 */
-	private Integer id;
+
 	/**
-	 * Getter of serialVersionUID
+	 * 
+	 * Die eindeutige Identifikationsnummer einer Instanz dieser Klasse.
+	 * 
 	 */
-	public Long getSerialVersionUID() {
-	 	 return serialVersionUID; 
+	private int id;
+
+	/**
+	 * 
+	 * Auslesen der ID.
+	 * 
+	 * @return
+	 */
+	public int getId() {
+		return id;
 	}
+
 	/**
-	 * Setter of serialVersionUID
+	 * 
+	 * Setzen der ID
+	 * 
+	 * @param id
 	 */
-	public void setSerialVersionUID(Long serialVersionUID) { 
-		 this.serialVersionUID = serialVersionUID; 
+	public void setId(int id) {
+		this.id = id;
 	}
+
 	/**
 	 * 
-	   * Auslesen der ID.
-	   
-	 * @return 
+	 * <p>
+	 * Feststellen der <em>inhaltlichen</em> Gleichheit zweier
+	 * <code>BusinessObject</code>-Objekte. Die Gleichheit wird in diesem
+	 * Beispiel auf eine identische ID beschränkt.
+	 * </p>
+	 * <p>
+	 * <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem Vergleich der
+	 * <em>Identität</em> eines Objekts mit einem anderen verwechseln!!! Dies
+	 * würde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
+	 * können Sie nocheinmal in die Definition des Sprachkerns von Java
+	 * schauen. Die Methode <code>equals(...)</code> ist für jeden
+	 * Referenzdatentyp definiert, da sie bereits in der Klasse
+	 * <code>Object</code> in einfachster Form realisiert ist. Dort ist sie
+	 * allerdings auf die simple Bestimmung der Gleicheit der Java-internen
+	 * Objekt-ID der verglichenen Objekte beschränkt. In unseren eigenen
+	 * Klassen können wir diese Methode überschreiben und ihr mehr Intelligenz
+	 * verleihen.
+	 * </p>
+	 * 
+	 * 
+	 * /**
+	 * 
+	 * <p>
+	 * Erzeugen einer ganzen Zahl, die für das <code>BusinessObject</code>
+	 * charakteristisch ist.
+	 * </p>
+	 * <p>
+	 * Zusammen mit <code>equals</code> sollte diese Methode immer definiert
+	 * werden. Manche Java-Klassen verwendenden <code>hashCode</code>, um
+	 * initial ein Objekt (z.B. in einer Hashtable) zu identifizieren. Erst
+	 * danach würde mit <code>equals</code> festgestellt, ob es sich
+	 * tatsächlich um das gesuchte Objekt handelt.
+	 * 
+	 * @return
 	 */
-	public Integer getId() {
-	 	 return id; 
-	}
+
 	/**
 	 * 
-	   * Setzen der ID
-	   
-	 * @param id 
-	 */
-	public void setId(Integer id) { 
-		 this.id = id; 
-	}
-	/**
+	 * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
+	 * Dies kann selbstverständlich in Subklassen überschrieben werden.
 	 * 
-	   * <p>
-	   * Feststellen der <em>inhaltlichen</em> Gleichheit zweier
-	   * <code>BusinessObject</code>-Objekte. Die Gleichheit wird in diesem Beispiel auf eine
-	   * identische ID beschränkt.
-	   * </p>
-	   * <p>
-	   * <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem Vergleich der
-	   * <em>Identität</em> eines Objekts mit einem anderen verwechseln!!! Dies
-	   * würde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
-	   * können Sie nocheinmal in die Definition des Sprachkerns von Java schauen.
-	   * Die Methode <code>equals(...)</code> ist für jeden Referenzdatentyp
-	   * definiert, da sie bereits in der Klasse <code>Object</code> in einfachster
-	   * Form realisiert ist. Dort ist sie allerdings auf die simple Bestimmung der
-	   * Gleicheit der Java-internen Objekt-ID der verglichenen Objekte beschränkt.
-	   * In unseren eigenen Klassen können wir diese Methode überschreiben und ihr
-	   * mehr Intelligenz verleihen.
-	   * </p>
-	   
-	 * @param o 
-	 * @return 
+	 * @return
 	 */
-	public Boolean equals(Object o) { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	   * <p>
-	   * Erzeugen einer ganzen Zahl, die für das <code>BusinessObject</code> charakteristisch ist.
-	   * </p>
-	   * <p>
-	   * Zusammen mit <code>equals</code> sollte diese Methode immer definiert werden. Manche Java-Klassen
-	   * verwendenden <code>hashCode</code>, um initial ein Objekt (z.B. in einer Hashtable) zu identifizieren. Erst danach
-	   * würde mit <code>equals</code> festgestellt, ob es sich tatsächlich um das gesuchte Objekt handelt.
-	   
-	 * @return 
-	 */
-	public Integer hashCode() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	   * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
-	   * Dies kann selbstverständlich in Subklassen überschrieben werden.
-	   
-	 * @return 
-	 */
-	public String toString() { 
-		// TODO Auto-generated method
-		return null;
-	 } 
+
 
 }
