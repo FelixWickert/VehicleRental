fab4996d6c2ab41f72dd10da80f18cabc10e0e36
Flex <fw038@hdm-stuttgart.de>
2017-05-13 20:13:35 +0200

    Klasse Vehicle angepasst- serialVersionUID + getter und setter entfernt

--- VehicleRental/src/de/hdm/VehicleRental/shared/bo/Vehicle.java
+++ VehicleRental/src/de/hdm/VehicleRental/shared/bo/Vehicle.java
@@ -1,10 +1,9 @@
 package de.hdm.VehicleRental.shared.bo;
 
-
 import java.lang.Object;
 
 public class Vehicle extends BusinessObject {
-
+	
 	/**
 	 * 
 	 */
@@ -34,7 +33,7 @@ public class Vehicle extends BusinessObject {
 	/**
 	 * 
 	 */
-	private static final Long serialVersionUID;
+
 	/**
 	 * 
 	 */
@@ -92,18 +91,7 @@ public class Vehicle extends BusinessObject {
 	public void setVehicleID(int vehicleID) { 
 		 this.vehicleID = vehicleID; 
 	}
-	/**
-	 * Getter of serialVersionUID
-	 */
-	public Long getSerialVersionUID() {
-	 	 return serialVersionUID; 
-	}
-	/**
-	 * Setter of serialVersionUID
-	 */
-	public void setSerialVersionUID(Long serialVersionUID) { 
-		 this.serialVersionUID = serialVersionUID; 
-	}
+
 	/**
 	 * 
 	 * @return 
@@ -160,32 +148,6 @@ public class Vehicle extends BusinessObject {
 	public void setVehiclePerformance(String vehiclePerformance, int vehicleID) { 
 		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	   * <p>
-	   * Feststellen der <em>inhaltlichen</em> Gleichheit zweier Account-Objekte.
-	   * Die Gleichheit wird in diesem Beispiel auf eine identische Kontonummer
-	   * beschränkt.
-	   * </p>
-	   * <p>
-	   * <b>ACHTUNG:</b> Die inhaltliche Gleichheit nicht mit dem Vergleich der
-	   * <em>Identität</em> eines Objekts mit einem anderen verwechseln!!! Dies
-	   * würde durch den Operator <code>==</code> bestimmt. Bei Unklarheit hierzu
-	   * können Sie nocheinmal in die Definition des Sprachkerns von Java schauen.
-	   * Die Methode <code>equals(...)</code> ist für jeden Referenzdatentyp
-	   * definiert, da sie bereits in der Klasse <code>Object</code> in einfachster
-	   * Form realisiert ist. Dort ist sie allerdings auf die simple Bestimmung der 
-	   * Gleicheit der Java-internen Objekt-ID der verglichenen Objekte beschränkt.
-	   * In unseren eigenen Klassen können wir diese Methode überschreiben und ihr
-	   * mehr Intelligenz verleihen.
-	   * </p>
-	   
-	 * @param o 
-	 * @return 
-	 */
-	public Boolean equals(Object o) { 
-		// TODO Auto-generated method
-		return null;
-	 } 
+
 
 }
