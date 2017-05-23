94c76fd784b69bada393cf9933b57b3ec3fcb612
Ulrieke Dieterle <ud012@hdm-stuttgart.de>
2017-05-23 16:02:27 +0200

    Wenn man sonst nichts kann, muss man wenigstens per youtube das Projekt richtig einbinden können :( Zum heulen!!!!

--- VehicleRental/src/de/hdm/VehicleRental/server/db/Daten_eingeben.java
+++ VehicleRental/src/de/hdm/VehicleRental/server/db/Daten_eingeben.java
@@ -1,20 +1,20 @@
-package de.hdm.VehicleRental.server;
+package de.hdm.VehicleRental.server.db;
+
+import java.util.Scanner;
 
 public class  Daten_eingeben { 
 	
 	public static void main {
 		
-		public einlesen{
-			
-			system.out.println("Geben Sie eine Automarke ein:");
+			System.out.println("Geben Sie eine Automarke ein:");
 			Scanner scanner1 = new Scanner(System.in);
 			String marke = scanner1.nextLine();
 			
-			system.out.println("Geben Sie eine Fahrzeugtyp an:");
+			System.out.println("Geben Sie eine Fahrzeugtyp an:");
 			Scanner scanner2 = new Scanner (System.in);
 			String typ = scanner2.nextLine();
 			
-			system.out.println("Geben Sie die Leistung des Fahrzeugs an:");
+			System.out.println("Geben Sie die Leistung des Fahrzeugs an:");
 			Scanner scanner3 = new Scanner (System.in);
 			String leistung = scanner3.nextLine();
 			
@@ -23,14 +23,14 @@ public class  Daten_eingeben {
 			vehicle.addTyp = typ;
 			vehicle.addLeistung = leistung;
 			
-			system.out.println(vehicle);
+			System.out.println(vehicle);
 		}
-		/*
+		
+	}
+/*
 		public ausgeben (){
 			
 			
 			system.out.println (vehicle);
 		}*/
-	}
-
 }
