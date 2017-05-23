c727d135fe244cd34b95e0245ed596243e4437ff
Ulrieke Dieterle <ud012@hdm-stuttgart.de>
2017-05-23 15:29:57 +0200

    einfach ignorieren!!

--- VehicleRental/src/de/hdm/VehicleRental/server/db/Daten_eingeben.java
+++ VehicleRental/src/de/hdm/VehicleRental/server/db/Daten_eingeben.java
@@ -1,29 +1,33 @@
 package de.hdm.VehicleRental.server;
 
+import java.util.Scanner;
+
+import testing.Vehicle;
+
 public class  Daten_eingeben { 
 	
 	public static void main {
 		
 		public einlesen{
 			
-			system.out.println("Geben Sie eine Automarke ein:");
+			System.out.println("Geben Sie eine Automarke ein:");
 			Scanner scanner1 = new Scanner(System.in);
 			String marke = scanner1.nextLine();
 			
-			system.out.println("Geben Sie eine Fahrzeugtyp an:");
+			System.out.println("Geben Sie eine Fahrzeugtyp an:");
 			Scanner scanner2 = new Scanner (System.in);
 			String typ = scanner2.nextLine();
 			
-			system.out.println("Geben Sie die Leistung des Fahrzeugs an:");
+			System.out.println("Geben Sie die Leistung des Fahrzeugs an:");
 			Scanner scanner3 = new Scanner (System.in);
 			String leistung = scanner3.nextLine();
 			
-			Vehicle vehicle = new Vehicle;
+			Vehicle vehicle = new Vehicle();
 			vehicle.addMarke = marke;
 			vehicle.addTyp = typ;
 			vehicle.addLeistung = leistung;
 			
-			system.out.println(vehicle);
+			System.out.println(vehicle);
 		}
 		/*
 		public ausgeben (){
