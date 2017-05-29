82c9a9055126adb62edfefa06c26fd38e69fea17
Carolin Tröster <ct019@hdm-stuttgart.de>
2017-05-29 16:07:10 +0200

    Doppelte OnMOduleLoad gelöscht

--- VehicleRental/src/de/hdm/VehicleRental/client/VehicleRental.java
+++ VehicleRental/src/de/hdm/VehicleRental/client/VehicleRental.java
@@ -64,44 +64,6 @@ public class VehicleRental implements EntryPoint {
 	  	   * <code>main()</code>-Methode normaler Java-Applikationen.
 	  	   
 	  	 */
-	  	public void onModuleLoad() { 
-	  		createLoginScreen();
-	  	 } 
-	  	
-	  	 private void createLoginScreen() {
-	  	      
-	  	      //CSS Stylename für die entsprechenden Divs setzen und mit Texten füllen
-	  	      FlowPanel splashContaiern = new FlowPanel();
-	  	      splashContaiern.setStyleName("splash-container");
-	  	      
-	  	      FlowPanel splash = new FlowPanel();
-	  	      splash.setStyleName("splash");
-
-	  	      HTML headingElement = new HTML();
-	  	      headingElement.setHTML("muh");
-	  	      headingElement.setStyleName("splash-head");
-
-	  	      FlowPanel splashSubhead = new FlowPanel(ParagraphElement.TAG);
-	  	      splashSubhead.setStyleName("splash-subhead");
-	  	      HTML splahParagraph = new HTML("Melde dich an und finde deine bessere Hälfte");
-	  	      splashSubhead.add(splahParagraph);
-
-	  	      Anchor loginAnchor = new Anchor("Los!");
-	  	      loginAnchor.setStyleName("pure-button-login pure-button-primary-login");
-
-
-	  	      //Divs und Anchor zum Div hinzufügen
-	  	      splash.add(headingElement);
-	  	      splash.add(splashSubhead);
-	  	      splash.add(loginAnchor);
-	  	      
-	  	      splashContaiern.add(splash);
-	  	      RootPanel.get("Details").add(splashContaiern);
-	  	      
-	  	     
-	  	    }
-
-	  }
 
 	      RootPanel.get("Details").add(splashContaiern);
 	      
